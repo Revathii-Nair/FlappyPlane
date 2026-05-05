adbc3ed8c123b98a0b715b21ee833aa0815717da
Revathi Nair <revathiinair2000@gmail.com>
2026-05-06 02:22:35 +0530

    Added game assets

---
+++ Scenes/Game/Game.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class Game : Node
+{
+	public override void _Ready(){}
+	public override void _Process(double delta){}
+}
---
+++ Scenes/Plane/Plane.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class Plane : CharacterBody2D
+{
+    public override void _Ready(){}
+	public override void _PhysicsProcess(double delta){}
+}
